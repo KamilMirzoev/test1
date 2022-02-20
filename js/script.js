7da5110eef16f93ad18490b955f3959cbe937bf3
KamilMirzoev <mirzoevk21@gmail.com>
2022-02-20 10:16:57 +0400

    end

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strick";
+
+var num1=30
+
+console.log(num1)
+
+var num2=5
+
+console.log(num2)
+
+console.log(num1+num2)
+
+console.log(num1-num2)
+
+console.log(num1*num2)
+
+console.log(num1/num2)
